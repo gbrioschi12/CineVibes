352687f9898400e715b3a3f16db34c8eda875833
Giorgia Brioschi <g.brioschi12@campus.unimib.it>
2024-12-28 12:27:15 +0100

    home

--- app/src/main/java/com/example/cinevibes/SchedaFilmActivity.java
+++ app/src/main/java/com/example/cinevibes/SchedaFilmActivity.java
@@ -15,10 +15,6 @@ public class SchedaFilmActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_scheda_film);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
