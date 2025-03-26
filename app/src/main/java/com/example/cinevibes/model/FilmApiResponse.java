510c0d703bee58aaeefbd0ffb9d3ef367d0a023f
degaale0 <164371008+degaale0@users.noreply.github.com>
2025-03-26 17:24:02 +0100

    Esercitazione 8: Una nuova speranza parte 1

--- app/src/main/java/com/example/cinevibes/model/FilmApiResponse.java
+++ app/src/main/java/com/example/cinevibes/model/FilmApiResponse.java
@@ -10,26 +10,32 @@ public class FilmApiResponse {
 
 
     public int getPage() {
+
         return page;
     }
 
     public void setPage(int page) {
+
         this.page = page;
     }
 
     public List<Film> getResults() {
+
         return results;
     }
 
     public void setResults(List<Film> results) {
+
         this.results = results;
     }
 
     public int getTotal_pages() {
+
         return total_pages;
     }
 
     public void setTotal_pages(int total_pages) {
+
         this.total_pages = total_pages;
     }
 
